7ac0ccaf130fdc62ec428bfd65e456745fa8f96c
Sam Allen <samallen659@gmail.com>
2023-04-28 19:15:36 +0100

    completed part 1

--- 1/index.js
+++ 1/index.js
@@ -3,8 +3,42 @@ const fs = require("fs");
 class Elf {
   constructor(foodList) {
     this.foodList = foodList;
+    this.calorieTotal = 0;
+  }
+  calculateCalorieTotal() {
+    let tempTotal = 0;
+    for (let i = 0; i < this.foodList.length; i++) {
+      const foodCalories = Number(this.foodList[i]);
+      if (typeof foodCalories === "number") tempTotal += foodCalories;
+    }
+    this.calorieTotal = tempTotal;
   }
 }
+
+class ElfContainer {
+  constructor() {
+    this.elfList = [];
+    this.highestCalorieTotal = 0;
+  }
+
+  addElf(elf) {
+    this.elfList.push(elf);
+  }
+  calculateElvesTotalCalories() {
+    for (let i = 0; i < this.elfList.length; i++) {
+      this.elfList[i].calculateCalorieTotal();
+    }
+  }
+  calculateHighestCalorieTotal() {
+    for (let i = 0; i < this.elfList.length; i++) {
+      if (this.highestCalorieTotal < this.elfList[i].calorieTotal) {
+        this.highestCalorieTotal = this.elfList[i].calorieTotal;
+      }
+    }
+  }
+}
+
+const elfContainer = new ElfContainer();
 
 function getFoodData(path) {
   fs.readFile(path, "ascii", (err, data) => {
@@ -19,12 +53,11 @@ function splitElfList(data) {
   const elfList = data.split("\n\n").map((food) => {
     const foodList = splitFoodList(food);
     const elf = new Elf(foodList);
-    return elf;
+    elfContainer.addElf(elf);
   });
-  printElfList(elfList);
-}
-function printElfList(elfList) {
-  console.log(elfList);
+  elfContainer.calculateElvesTotalCalories();
+  elfContainer.calculateHighestCalorieTotal();
+  console.log(elfContainer.highestCalorieTotal);
 }
 function splitFoodList(data) {
   return data.split("\n");
