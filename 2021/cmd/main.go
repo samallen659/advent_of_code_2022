61f85bb20968efb571a48c84a6c037c40dfc8ec9
Sam Allen <samallen659@gmail.com>
2023-07-16 20:03:36 +0100

    day1 complete

--- 2021/cmd/main.go
+++ 2021/cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/samallen659/advent_of_code/2021/pkg/days/day1"
 )
 
 func main() {
-	day1.Day1()
+	fmt.Printf("%d\n", day1.Part1())
+	fmt.Printf("%d\n", day1.Part2())
 }
